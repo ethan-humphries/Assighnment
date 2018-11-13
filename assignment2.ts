9369c5f44bf1837664cf45a7a27a2477fa480d19
Ethan Humphries <40162550+ethan-humphries@users.noreply.github.com>
2018-11-13 23:57:05 +1100

    Add files via upload

--- assignment2.ts
+++ assignment2.ts
@@ -6,64 +6,83 @@ class Vehicle {
     transmission :  string;
     bodyShape : string;
 
-    constuctor(r:  string, m:  string, mdl: string, trans:  string, body: string , y?: number) {
-        this.registration = r;
-        this.make = m;
-        this.model = mdl;
-        this.year =  y;
-        this.transmission=  trans;
-        this.bodyShape = body;
+    constructor(registration:  string, make:  string, model: string, transmission:  string, bodyShape: string , year?: number) {
+        this.registration = registration;
+        this.make = make;
+        this.model = model;
+        this.year =  year;
+        this.transmission=  transmission;
+        this.bodyShape = bodyShape;
     }
 
     getRegistration() : string {
         return this.registration;
     }
 
-    setRegistration(r : string) {
-        this.registration =  r;
+    setRegistration(registration : string) {
+        this.registration =  registration;
     }
 
     getMake() : string {
         return this.make;
     }
 
-    setMake(m : string) {
-        this.make = m;
+    setMake(make : string) {
+        this.make = make;
     }
 
     getModel() : string {
         return this.model;
     }
 
-    setModel(mdl : string) {
-        this.model = mdl;
+    setModel(model : string) {
+        this.model = model;
     }
 
     getTransmission() : string {
         return this.transmission;
     }
 
-    setTransmission(trans : string) {
-        this.transmission = trans;
+    setTransmission(transmission : string) {
+        this.transmission = transmission;
     }
 
     getBodyshape() : string {
         return this.bodyShape;
     }
 
-    setBodyShape(body : string) {
-        this.bodyShape = body;
+    setBodyShape(bodyShape : string) {
+        this.bodyShape = bodyShape;
     }
 
     getYear() : number {
         return this.year;
     }
 
-    setYear(y : number) {
-        this.year = y;
+    setYear(year : number) {
+        this.year = year;
     }
 
 }
 
+let vehicles: Array<Vehicle>
+function initializeVehicles() : void {
+vehicles = [
+    new Vehicle("ABC123", "Honda", "Accord", "Manual", "Sedan", 2001),
+    new Vehicle("BBA124", "Toyota", "Yaris", "Automatic", "Sedan", 2005),
+    new Vehicle("DDD100", "Ford", "Falcon", "Manual", "Ute", 2004),
+    new Vehicle("ABC111", "Honda", "Accord", "Automaticl", "Sedan", 2010),
+    new Vehicle("CIC333", "Toyota", "Accord", "Manual", "Sedan", 2018),
+    new Vehicle("ABC145", "Ford", "F250", "Manual", "Ute", 2000),
+    new Vehicle("KBC156", "Ford", "Focus", "Automatic", "Sedan", 1995),
+    new Vehicle("ABC178", "Toyota", "Yaris", "Manual", "Sedan", 1996),
+    new Vehicle("HBC121", "Honda", "Accord", "Manual", "Sedan", 2001),
+    new Vehicle("ADC173", "Honda", "Accord", "automatic", "Sedan", 2012)  
+]  
+}
 
+initializeVehicles();
 
+for (let vehicle of vehicles){
+    console.log(vehicle.getRegistration());
+}
